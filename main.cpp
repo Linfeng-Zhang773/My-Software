bb121e8a2e232bd4b6da5fd40a0d9d4935fb0413
Linfeng-Zhang773 <linfengzh01@gmail.com>
2023-12-01 15:16:07 -0800

    ..

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
-#include <LTF.h>
-#include <iomanip>
-#include <iostream>
-
-using namespace std;
+#include "includes/Application/Application.h"
+#include "includes/DropdownMenu/DropdownMenu.h"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+
+    DropdownMenu menu;
+    Application::addComponent(menu);
+    // menu.setBoxPosition({100, 50}); // be able to change position
+
+    menu.addItem("Apple");
+    menu.addItem("Banana");
+    menu.addItem("Car");
+    menu.addItem("Dog");
+    menu.addItem("Elf");
+    // menu.changeSize({300, 30}); // be able to resize
+    Application::run();
+
     return 0;
 }
